a2d6d59cf35e6420a39a60b6ccb39b5a4f0af1ed
balanarasimhulu <66964236+balanarasimhulu@users.noreply.github.com>
2022-05-30 07:36:26 +0530

    Model files

---
+++ Movie.java
@@ -0,0 +1,34 @@
+package com.example.moviecatalogservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private String movieId;
+    public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String name;
+    private String description;
+}
